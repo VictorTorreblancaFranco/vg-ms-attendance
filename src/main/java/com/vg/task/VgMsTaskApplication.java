f26f51bf7e4167968fdc5a81666d0b8f5b9c480d
Carlos Caycho <carlos.caycho.casas@vallegrande.edu.pe>
2026-04-29 18:24:10 -0500

    chore: subir cambios locales a develop

---
+++ src/main/java/com/vg/task/VgMsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.vg.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgMsTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgMsTaskApplication.class, args);
+	}
+
+}
