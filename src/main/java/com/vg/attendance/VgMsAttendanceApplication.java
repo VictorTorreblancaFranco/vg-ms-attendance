66b9643e2a498049c3a7c9a80690f4d40d580f30
Victor Torreblanca <victor.torreblanca.franco@vallegrande.edu.pe>
2026-05-31 13:50:08 -0500

    feat: add hexagonal architecture structure for attendance microservice

---
+++ src/main/java/com/vg/attendance/VgMsAttendanceApplication.java
@@ -0,0 +1,19 @@
+package com.vg.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class VgMsAttendanceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VgMsAttendanceApplication.class, args);
+    }
+}
